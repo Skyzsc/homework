6732373c4a1a1d25411c9aff75f2756dd0a9e6f1
stu <stu@qq.com>
2016-10-31 18:10:45 +0800

    博客（不完整版）

--- 第一次work/Controllers/HomeController.cs
+++ 第一次work/Controllers/HomeController.cs
@@ -30,9 +30,18 @@ namespace 第一次work.Controllers
         /// 新闻列表
         /// </summary>
         /// <returns></returns>
-        public ActionResult News()
+        public ActionResult List(int page = 10)
         {
-            ViewData["Name"] = "Bruce";
+            string[] data = new string[] { "六中全会夯实从严治党政治根基 治国理政",
+                                           "解读：加强党内监督如何监督 主要监督谁" ,
+                                           "李克强:减税降费同样是积极财政政策 中国经济被看好",
+                                           "洪秀柱拜谒中山陵并讲话：国民党坚定反“台独”"};
+
+            ViewBag.data = data;
+            ViewBag.Page = page;
+
+            //ViewData["data"] = data;
+            //ViewData.Model = data;
             return View();
         }
         /// <summary>
